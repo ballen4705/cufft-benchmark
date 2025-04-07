54db2fa50ceb21d7b1be940cd4702a5e64cb842a
Bruce Allen <bruce.allen@aei.mpg.de>
2025-04-07 17:00:06 +0000

    A bit of final code cleanup

--- cufft_benchmark.cu
+++ cufft_benchmark.cu
@@ -29,9 +29,10 @@ do { \
 #define LINE_LEN 128
 int load_lengths(const char* filename, int* ntrials, long long** nffts, char (**description)[LINE_LEN]) {
     int count = 0;
-    char buffer[256];
+    char buffer[LINE_LEN];
     FILE* f = fopen(filename, "r");
     if (!f) {
+       fprintf(stderr, "Failed to read text file %s containing lengths for different trials.\n", filename);
        perror(filename);
        return 1;
     }
@@ -39,8 +40,8 @@ int load_lengths(const char* filename, int* ntrials, long long** nffts, char (**
     while (fgets(buffer, sizeof(buffer), f)) count++;
     *ntrials = count; 
     // Allocate memory
-    *nffts = (long long *)malloc(count * sizeof(long long));
-    *description = (char (*)[LINE_LEN])malloc(count * LINE_LEN * sizeof(char));
+    *nffts = (long long*) malloc(count * sizeof(**nffts));
+    *description = (char (*)[LINE_LEN]) malloc(count * sizeof(**description));
     if (!*nffts || !*description) {
         fprintf(stderr, "Memory allocation failed\n");
         fclose(f);
@@ -51,7 +52,7 @@ int load_lengths(const char* filename, int* ntrials, long long** nffts, char (**
     for (int i = 0; i < count; i++) {
         if (!fgets(buffer, sizeof(buffer), f)) break;
         sscanf(buffer, "%lld =", &((*nffts)[i]));
-        strncpy((*description)[i], buffer, LINE_LEN - 1);
+        strncpy((*description)[i], buffer, LINE_LEN);
         (*description)[i][LINE_LEN - 1] = '\0';
     }
     fclose(f);
@@ -94,54 +95,59 @@ int main(int argc, char **argv) {
        fprintf(stderr, "Failed to load Lengths.txt\n");
        exit(1);
     }
-    printf("Testing %d FFT lengths, ranging from %d to %d.\n", ntrials, nffts[0], nffts[ntrials-1]);
-        
+    printf("Testing %d FFT lengths, ranging from %lld to %lld.\n", ntrials, nffts[0], nffts[ntrials-1]);
+    srand(time(NULL)); // initialize seed
+
+    float* times = (float*) malloc(num_iterations * sizeof(*times));
+    if (!times) {
+        fprintf(stderr, "Failed to allocate timing buffer\n");
+        return 1;
+    }
+
+    // Loop over the different FFT trial lengths
     for (int i = 0; i < ntrials; i++){
         long long n = nffts[i];
         fprintf(stderr,"**************************************\n");
         fprintf(stderr,"N-point FFT: %s\n", description[i]);
-        fprintf(stderr,"Number of iterations: %lld \n", num_iterations);
+        fprintf(stderr,"Number of iterations: %lld\n", num_iterations);
 
         int batch = 1;
         int rank = 1;
-        long long nembed[1] = {n};
-        int istride = 1;
-        int ostride = 1;
-        long long idist = n;
-        long long odist = n;
-        long long inembed[1] = {n};
-        long long onembed[1] = {n};
+        int istride = 1, ostride = 1;
+        long long idist = n, odist = n;
+        long long nembed[1] = {n}, inembed[1] = {n}, onembed[1] = {n};
         cufftHandle forward_plan;
         cudaEvent_t start, stop;
-        float elapsed_time;
-        float *input_data;
+        float *input_data, *host_input_data;
         cufftComplex *fft_data;
-        float *host_input_data;
         float mean_time, median_time;
         size_t work_size;
 
-        float input_size_gb = n*4.0/1e9;
-        fprintf(stderr,"Input float array size: %lf GB \n", input_size_gb);
-        float output_size_gb = n*8.0/1e9;
-        fprintf(stderr,"Output complex array size: %lf GB \n", output_size_gb);
+	float input_size_gb = n * sizeof(float)/1e9;
+	float output_size_gb = (n/2 + 1)*sizeof(cufftComplex)/1e9;
+        fprintf(stderr,"Input float array size: %lf GB\n", input_size_gb);
+        fprintf(stderr,"Output complex array size: %lf GB\n", output_size_gb);
 
         // Allocate memory on host
-        host_input_data = (float*) malloc(n * batch * sizeof(float));
+	host_input_data = (float*) malloc(n * batch * sizeof(*host_input_data));
+	if (!host_input_data) {
+	   fprintf(stderr, "Failed to allocate input data array\n");
+	   exit(1);
+	}
 
         // Initialize input data on host
-        srand(time(NULL));
         for (long int k = 0; k < n * batch; k++) {
             host_input_data[k] = (float) rand() / RAND_MAX;
         }
         //get size estimate
-        cufftResult result = cufftEstimate1d(n, CUFFT_R2C, batch, &work_size);
+	CHECK_CUFFT_ERROR(cufftEstimate1d(n, CUFFT_R2C, batch, &work_size));
         float work_size_gb = work_size/1.0e9;
         fprintf(stderr, "Work size estimate: %lf GB\n", work_size_gb);
         fprintf(stderr, "Total size estimate: %lf GB\n", input_size_gb + output_size_gb + work_size_gb);
 
         // Allocate memory on device
-        CHECK_CUDA_ERROR(cudaMalloc((void**) &input_data, n * batch * sizeof(float)));
-        CHECK_CUDA_ERROR(cudaMalloc((void**) &fft_data, (n/2 + 1) * batch * sizeof(cufftComplex)));
+	CHECK_CUDA_ERROR(cudaMalloc((void**) &input_data, n * batch * sizeof(*input_data)));
+	CHECK_CUDA_ERROR(cudaMalloc((void**) &fft_data, (n/2 + 1) * batch * sizeof(*fft_data)));
 
         // Create FFT plan
         CHECK_CUFFT_ERROR(cufftCreate(&forward_plan));
@@ -150,14 +156,12 @@ int main(int argc, char **argv) {
         // Copy input data to device
         CHECK_CUDA_ERROR(cudaMemcpy(input_data, host_input_data, n * batch * sizeof(float), cudaMemcpyHostToDevice));
 
-        mean_time = 0.0;
-
         // Record FFT times
-        float times[num_iterations];
+        mean_time = 0.0;
+	CHECK_CUDA_ERROR(cudaEventCreate(&start));
+	CHECK_CUDA_ERROR(cudaEventCreate(&stop));
         for (int iter = 0; iter < num_iterations; iter++) {
-            elapsed_time = 0.0;
-            CHECK_CUDA_ERROR(cudaEventCreate(&start));
-            CHECK_CUDA_ERROR(cudaEventCreate(&stop));
+            float elapsed_time = 0.0;
             CHECK_CUDA_ERROR(cudaEventRecord(start, 0));
 
             CHECK_CUFFT_ERROR(cufftExecR2C(forward_plan, input_data, fft_data));
@@ -168,7 +172,10 @@ int main(int argc, char **argv) {
             mean_time += elapsed_time;    
             times[iter] = elapsed_time;    
         }
+	CHECK_CUDA_ERROR(cudaEventDestroy(start));
+	CHECK_CUDA_ERROR(cudaEventDestroy(stop));
 
+	// compute median times and mean
 	qsort(times, num_iterations, sizeof(float), compare_floats);
 	if (num_iterations % 2 == 0) {
 	   median_time = (times[num_iterations / 2 - 1] + times[num_iterations / 2]) / 2.0f;
@@ -186,6 +193,5 @@ int main(int argc, char **argv) {
         CHECK_CUDA_ERROR(cudaFree(fft_data));
         CHECK_CUFFT_ERROR(cufftDestroy(forward_plan));
     }
-
     return 0;
 }
